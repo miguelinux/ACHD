41f994299107d93e3c16727359bc07d1cbc95c76
AngelRysu <jesus.quecam@gmail.com>
2024-02-21 15:39:39 -0600

    se mejora la calificacion de pylint de 7 a 9

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+
 # c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
 # vi: set shiftwidth=4 tabstop=8 expandtab:
 # :indentSize=4:tabSize=8:noTabs=true:
@@ -5,16 +6,11 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import hashlib
-import string
-import secrets
 import json
-
-from datetime import datetime
-from datetime import timedelta
+from configparser import ConfigParser
 
 from flask import Flask, render_template, send_from_directory
-from flask import session, request, make_response, redirect,url_for
-from configparser import ConfigParser
+from flask import session, request, redirect
 from flask_orator import Orator
 
 
@@ -23,7 +19,8 @@ app=Flask(__name__)
 config = ConfigParser()
 config.read(".env") # EN ESTE APARTADO TOMARA LOS VALORES DE ENTORNO OBTENIDOS EN .env
 
-app.secret_key = config.get("SCT","SECRET_KY") #para guardar las cookies es necesario una secret_key que se encuentra en .env
+app.secret_key = config.get("SCT","SECRET_KY") 
+#para guardar las cookies es necesario una secret_key que se encuentra en .env
 
 DB_HOST = config.get("DB", "DB_HOST")
 DB_PASSWORD = config.get("DB", "DB_PASSWORD")
@@ -57,16 +54,13 @@ def get_hex_digest(cadena):
     sha256 = hashlib.sha256()
     sha256.update(cadena.encode("utf-8"))
     hexdigest = sha256.hexdigest()
-
     return hexdigest
 
 def verificate_session():
     if 'username' in session:
         user = session["username"]
         return user
-    else:
-        return False
-
+    return False
 
 @app.route("/img/<filename>", methods=["GET"])
 def route_img_files(filename):
@@ -76,7 +70,6 @@ def route_img_files(filename):
     """
     return send_from_directory("templates/img", path=filename)
 
-
 @app.route("/css/<filename>")
 def route_js_files(filename):
     """
@@ -84,7 +77,6 @@ def route_js_files(filename):
     Regresa el archivo CSS.
     """
     return send_from_directory("templates/css", path=filename)
-
 
 @app.route("/js/<filename>")
 def route_css_files(filename):
@@ -94,14 +86,12 @@ def route_css_files(filename):
     """
     return send_from_directory("templates/js", path=filename)
 
-
 @app.route("/")
 def index():
     """
     Si hay una sesión iniciada, redirecciona al dashboard.
     Si no hay una sesión iniciada, regresa el login.
     """
-
     return render_template("index.html")
 
 @app.route("/logout")
@@ -123,15 +113,14 @@ def login():
     form = request.form
     print(str(form))
     user = form["email"]
-    pssw = form["password"]
+    pssw = get_hex_digest(form["password"])
     user = (
         db.table("usersPrueba")
         .where("email", user)
-        .where("password", get_hex_digest(pssw))
+        .where("password", pssw)
         .get()
         .first()
     )
-
     if user is None:
         return render_template(
             "index.html", mensaje="Usuario y/o contraseña incorrectos"
@@ -148,9 +137,7 @@ def dashboard():
     user=verificate_session()
     if user:
         return render_template("dashboard.html", user=user)
-    else:
-        return redirect('/')
-
+    return redirect('/')
 
 @app.route("/homeDocente")
 def home_docente():
@@ -159,9 +146,8 @@ def home_docente():
     """
     user=verificate_session()
     if user:
-        return render_template("homeDocente.html", user=user)
-    else:
-        return redirect('/')
+        return render_template("homeDocente.html", user=user)    
+    return redirect('/')
 
 @app.route("/horario")
 def horario():
@@ -173,10 +159,8 @@ def horario():
         usuario = db.table("usersPrueba").where("user",user).get().first()
         disponibilidad = usuario.disponibilidad
         return render_template("horario.html", user=user, disponibilidad=disponibilidad)
-    else:
-        return redirect('/')
-        
-
-
+    return redirect('/')
+    
 if __name__=='__main__':
     app.run(debug=True, port=5000)
+    
