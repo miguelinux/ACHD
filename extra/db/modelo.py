2de04aa382083b20a7df80613a9846de8ddba772
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2024-04-11 21:57:47 -0600

    iniciando con la estructura de la BBDD

---
+++ extra/db/modelo.py
@@ -0,0 +1,29 @@
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""
+Modelo de la base de datos con SQLAlchemy
+"""
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+
+# declarative base class
+class Base(DeclarativeBase):
+    pass
+
+
+# an example mapping using the base
+class Usuario(Base):
+    __tablename__ = "usuario"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str]
+    apellido_pat: Mapped[str] = mapped_column(String(30))
+    apellido_mat: Mapped[Optional[str]]
